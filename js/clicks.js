b43e943aaaf94ad2ec887175b88df60fcb8981c3
Danika Harada <danimi65@gmail.com>
2017-01-07 16:12:53 -1000

    got different clicks to go to different websites

---
+++ js/clicks.js
@@ -0,0 +1,13 @@
+document.addEventListener('contextmenu', (event) =>{
+  event.preventDefault();
+  window.location.href = "https://www.pinterest.com/";
+});
+
+document.addEventListener('click', (event) =>{
+  event.preventDefault();
+  // alert("NOPE");
+  // rightClick();
+  window.location.href = "http://www.google.com";
+
+
+});
